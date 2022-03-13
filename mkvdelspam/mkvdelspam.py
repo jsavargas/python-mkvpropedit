d439a7108e9c86b680ab62c02c91a9e17e6460f6
jsavargas <jsavargas@gmail.com>
2022-03-13 01:19:12 -0300

    update mkvdelspam.py

--- mkvdelspam/mkvdelspam.py
+++ mkvdelspam/mkvdelspam.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
-from pprint import pprint
-from pymediainfo import MediaInfo
 
+try:
+    from pymediainfo import MediaInfo
+    flag_pymediainfo = True
+except:
+    print("NO pymediainfo ")
+    flag_pymediainfo = False
+
+
+import subprocess
 import sys
 import os
-
+import re
 
 
 n = len(sys.argv)
@@ -18,40 +25,78 @@ print(mediafile)
 print("")
 print("")
 
-media_info = MediaInfo.parse(mediafile)
 
-command = []
-audio = 0
-subs = 0
+if flag_pymediainfo:
+    media_info = MediaInfo.parse(mediafile)
 
-for track in media_info.tracks:
+    command = []
+    audio = 0
+    subs = 0
 
-    if track.track_type == "Video":
-        print("Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, "
-              "Format: {t.format}".format(t=track)
-        )
-    elif track.track_type == "Audio":
-        audio += 1
-        print("Audio",track.track_type, track.track_id, track.title, track.language)
+    for track in media_info.tracks:
 
-        command.append(f'--edit track:a{audio} --delete name ')
+        if track.track_type == "Video":
+            print("Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, "
+                "Format: {t.format}".format(t=track)
+            )
+        elif track.track_type == "Audio":
+            audio += 1
+            print("Audio",track.track_type, track.track_id, track.title, track.language)
 
-    elif track.track_type == "Text":
-        subs += 1
-        print("Audio",track.track_type, track.track_id, track.title, track.language)
-        command.append(f'--edit track:s{subs} --delete name ')
-command.append(f' --edit track:v1 --delete name ')
+            command.append(f'--edit track:a{audio} --delete name ')
 
-print("")
-print("")
-#print(command)
+        elif track.track_type == "Text":
+            subs += 1
+            print("Audio",track.track_type, track.track_id, track.title, track.language)
+            command.append(f'--edit track:s{subs} --delete name ')
+    command.append(' --edit track:v1 --delete name ')
 
-mkvpropedit = "mkvpropedit --tags all: --delete title --delete-attachment mime-type:image/jpeg --delete-attachment 1 --edit track:v1 --delete name {} '{}'".format("".join(command),mediafile)
-print(mkvpropedit)
-print(os.system(mkvpropedit))
+    print("")
+    print("")
+    #print(command)
+
+    mkvpropedit = "mkvpropedit --tags all: --delete title --delete-attachment mime-type:image/jpeg --delete-attachment 1  {} '{}'".format("".join(command),mediafile)
+    print(mkvpropedit)
+    print(os.system(mkvpropedit))
 
 
-print("")
-#print(os.system(f"mediainfo {mediafile}"))
+    print("")
+    #print(os.system(f"mediainfo {mediafile}"))
+else:
+    mediaInfo = os.system("mediainfo {mediafile}".format(mediafile=mediafile))
+    print("mediaInfo",mediaInfo)
 
+    print("*" *80)
+    print("*" *80)
+
+    proc = subprocess.Popen("mediainfo '{mediafile}'".format(mediafile=mediafile), shell=True, stdout=subprocess.PIPE)
+
+    command = []
+    audio = 0
+    subs = 0
+
+    for line in proc.stdout:
+        #print(line)
+        if re.search('^Audio', line.decode()):
+            audio += 1
+            print(line.decode().replace('\n','') )
+            command.append(f'--edit track:a{audio} --delete name ')
+            #          command.append(f'--edit track:a{audio} --set name="Español" ')
+        if re.search('^Text', line.decode()):
+            subs += 1
+            print(line.decode().replace('\n','') )
+            command.append(f'--edit track:s{subs} --delete name ')
+        if re.search('^Title', line.decode()):
+            print(line.decode().replace('\n','') )
+        if re.search('^Language', line.decode()):
+            print(line.decode().replace('\n','') )
+
+    print("\n" *2)
+
+
+    command.append(' --edit track:v1 --delete name ')
+    mkvpropedit = "mkvpropedit --tags all: --delete title --delete-attachment mime-type:image/jpeg --delete-attachment 1 {} '{}'".format("".join(command),mediafile)
+    print(mkvpropedit)
+
+    print(os.system(mkvpropedit))
 
